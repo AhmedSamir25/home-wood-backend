c3de330708f4ebc9e56a04b75b051f5249b245ce
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-08-29 14:34:23 +0300

    products routing

---
+++ router/product_router.go
@@ -0,0 +1,11 @@
+package router
+
+import (
+	"homewood/controller"
+
+	"github.com/gofiber/fiber/v3"
+)
+
+func ProductsRouting(app *fiber.App) {
+	app.Get("homewood/products", controller.GetAllProducts)
+}
